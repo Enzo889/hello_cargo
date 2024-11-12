57a56d42a84e53d50bd4764bdd539160caf8d606
Enzo Bustamante <enzobustamante895@gmail.com>
2024-11-12 09:43:30 -0300

    feat: another test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
     println!("Hello, world!");
+
+    another_function();
+
+    add(12, 44);
+}
+
+fn another_function() {
+    println!("Another function.");
+}
+
+fn add(x: i32, y: i32) {
+    println!("{} + {} = {}", x, y, x + y);
 }
